6a8b3524953e4fe994840d9851238c7bfa172677
reddii7 <104622120+reddii7@users.noreply.github.com>
2025-05-06 16:07:41 +0100

    Update theme-switcher.js

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -17,7 +17,7 @@
 
     // Set default theme to 'light'.
     // Possible options: 'dark' or system color mode (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light')
-    return 'light'
+  return 'dark';
   }
 
   const setTheme = theme => {
